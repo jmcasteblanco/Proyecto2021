2c0b7cf4b51da1c95f211c5645cb38fbf2394242
jmcasteblanco <jmcasteblanco@misena.edu.co>
2021-06-28 22:26:44 -0500

    Se crea mas usuarios para la base de datos ,se crea comando para encriptar password

--- Proyecto2021/Controllers/UsuarioController.cs
+++ Proyecto2021/Controllers/UsuarioController.cs
@@ -1,5 +1,7 @@
 ﻿using Proyecto2021.Models;
+using System;
 using System.Linq;
+using System.Text;
 using System.Web.Mvc;
 
 namespace Proyecto2021.Controllers
@@ -15,6 +17,48 @@ namespace Proyecto2021.Controllers
                 return View(db.usuario.ToList());
             }
 
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuario usuario)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try { 
+                using (var db = new inventariop2021Entities())
+                {
+                    usuario.password = UsuarioController.HashSHA1(usuario.password);
+                    db.usuario.Add(usuario);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                 }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+
+            return sb.ToString();
         }
     }
 }
