3a257938a81152d32328719289dd420b1745497a
jmcasteblanco <jmcasteblanco@misena.edu.co>
2021-06-28 20:50:31 -0500

    Consultando datos de la tabla

---
+++ Proyecto2021/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using Proyecto2021.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Proyecto2021.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+
+        {
+            using (var db = new inventariop2021Entities())
+            {
+                return View(db.usuario.ToList());
+            }
+
+        }
+    }
+}
