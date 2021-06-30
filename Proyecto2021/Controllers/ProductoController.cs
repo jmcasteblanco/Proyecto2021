5151cf0cb292b1f13c773bc4138b0f7d2e8ff655
jmcasteblanco <jmcasteblanco@misena.edu.co>
2021-06-29 21:00:37 -0500

    editar,eliminar y detalles del producto

--- Proyecto2021/Controllers/ProductoController.cs
+++ Proyecto2021/Controllers/ProductoController.cs
@@ -63,6 +63,66 @@ namespace Proyecto2021.Controllers
             }
         }
 
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventariop2021Entities())
+            {
+                producto productoDetalle = db.producto.Where(a => a.id == id).FirstOrDefault();
+                return View(productoDetalle);
+            }
+        } 
 
+        public ActionResult Delete(int id)
+        {
+            using (var db = new inventariop2021Entities())
+            {
+                var productDelete = db.producto.Find(id);
+                db.producto.Remove(productDelete);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventariop2021Entities())
+                {
+                    producto producto = db.producto.Where(a => a.id == id).FirstOrDefault();
+                    return View(producto);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(producto productoEdit)
+        {
+            try
+            {
+               using (var db = new inventariop2021Entities())
+                {
+                    var producto = db.producto.Find(productoEdit.id);
+                    producto.nombre = productoEdit.nombre;
+                    producto.percio_unitario = productoEdit.percio_unitario;
+                    producto.cantidad = productoEdit.cantidad;
+                    producto.descripcion = productoEdit.descripcion;
+                    producto.id_proveedor = productoEdit.id_proveedor;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
     }
 }
