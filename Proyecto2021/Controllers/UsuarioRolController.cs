64cfa2b9b1c873610c5345eec33e8a743d754f1d
jmcasteblanco <jmcasteblanco@misena.edu.co>
2021-07-03 05:46:44 -0500

    Crud Usuario rol

---
+++ Proyecto2021/Controllers/UsuarioRolController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto2021.Models;
+
+namespace Proyecto2021.Controllers
+{
+    public class UsuarioRolController : Controller
+    {
+        // GET: UsuarioRol
+        public ActionResult Index()
+        {
+            using (var db = new inventariop2021Entities())
+            {
+                return View(db.usuariorol.ToList());
+            }
+                
+        }
+
+        public static string NombreUsuario(int idUsuario)
+        {
+            using (var db = new inventariop2021Entities())
+            {
+                return db.usuario.Find(idUsuario).nombre;
+            }
+        }
+
+        public ActionResult ListarUsuario()
+        {
+            using (var db = new inventariop2021Entities())
+            {
+                return PartialView(db.usuario.ToList());
+            }
+        }
+
+        public static string NombreRol(int idRol)
+        {
+            using (var db = new inventariop2021Entities())
+            {
+                return db.roles.Find(idRol).descripcion;
+            }
+        }
+
+        public ActionResult ListarRol()
+        {
+            using (var db = new inventariop2021Entities())
+            {
+                return PartialView(db.roles.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(usuariorol usuarioRol)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventariop2021Entities())
+                {
+                    db.usuariorol.Add(usuarioRol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventariop2021Entities())
+            {
+                usuariorol usuariorolDetalle = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                return View(usuariorolDetalle);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            using (var db = new inventariop2021Entities())
+            {
+                var usuariorolDelete = db.usuariorol.Find(id);
+                db.usuariorol.Remove(usuariorolDelete);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db= new inventariop2021Entities())
+                {
+                    usuariorol finduser = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                    return View(finduser);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(usuariorol usuarioRolEdit)
+        {
+            try
+            {
+                using (var db = new inventariop2021Entities())
+                {
+                    usuariorol usuarioRol = db.usuariorol.Find(usuarioRolEdit.id);
+                    usuarioRol.idUsuario = usuarioRolEdit.idUsuario;
+                    usuarioRol.idRol = usuarioRolEdit.idRol;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}   
